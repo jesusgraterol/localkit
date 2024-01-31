9bc8cf9fe31414fd5e1eb4376f4558d86ac8401a
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-01-31 18:56:02 -0400

    Refactor the Utilities Static Class in favor of Factory Functions
    This commit also includes the integration tests for the Utilities Singleton
    
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/modules/shared/utilities/utilities.js
+++ src/modules/shared/utilities/utilities.js
@@ -1,29 +1,40 @@
 import { format } from 'date-fns';
 
+
 /**
- * Utilities
+ * Utilities Factory
  * Provides commonly used functionality in order to main consistency across modules.
  */
-class Utilities {
+const utilitiesFactory = () => {
   /* **************
    * MISC HELPERS *
    ************** */
 
+
   /**
-   * Builds the header of the CLI based on the extracted version.
-   * @param {*} version
+   * Formats a timestamp or the current time if none is provided.
+   * @param {?} timestamp
+   * @param {?} dateFormat
    * @returns string
    */
-  static buildCLIHeader(version) {
+  const formatDate = (
+    timestamp = undefined,
+    dateFormat = 'dd/MM/yyyy, hh:mm:ss a',
+  ) => format(typeof timestamp === 'number' ? new Date(timestamp) : new Date(), dateFormat);
+
+
+  /**
+   * Builds the header of the CLI based on the extracted version.
+   * @param {?} version
+   * @returns string
+   */
+  const buildCLIHeader = (version) => {
     let header = 'LOCALKIT';
     if (typeof version === 'string') {
       header += ` v${version}`;
     }
     return `\n${header}\n`;
-  }
-
-
-
+  };
 
 
   /**
@@ -33,10 +44,10 @@ class Utilities {
    * @param {?} includeDate
    * @param {?} includeBorders
    */
-  static print(title, content, includeDate = false, includeBorders = true) {
+  const print = (title, content, includeDate = false, includeBorders = true) => {
     if (includeBorders) console.log('\n========================================\n');
     console.log(`${title}`);
-    if (includeDate) console.log(Utilities.formatDate());
+    if (includeDate) console.log(formatDate());
     console.log('\n');
     if (typeof content === 'string') {
       console.log(content);
@@ -48,33 +59,38 @@ class Utilities {
       throw new Error('The content must be a string or a string array.');
     }
     if (includeBorders) console.log('\n========================================');
-  }
-
-
-
-
-
-  /**
-   * Formats a timestamp or the current time if none is provided.
-   * @param {?} timestamp
-   * @param {?} dateFormat
-   * @returns string
-   */
-  static formatDate(timestamp = undefined, dateFormat = 'dd/MM/yyyy, hh:mm:ss a') {
-    return format(typeof timestamp === 'number' ? new Date(timestamp) : new Date(), dateFormat);
-  }
-
-
+  };
 
 
   /**
    * Creates a delay which duration is based on the amount of provided seconds.
    * @returns Promise<void>
    */
-  static delay(seconds) {
-    return new Promise((resolve) => { setTimeout(resolve, seconds * 1000); });
-  }
-}
+  const delay = (seconds) => new Promise((resolve) => { setTimeout(resolve, seconds * 1000); });
+
+
+
+
+  /* **************
+   * MODULE BUILD *
+   ************** */
+  return {
+    // misc helpers
+    formatDate,
+    buildCLIHeader,
+    print,
+    delay,
+  };
+};
+
+
+
+
+/**
+ * Global Instance
+ */
+const Utilities = utilitiesFactory();
+
 
 
 
