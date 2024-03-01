c1479bf2b9aee26cfec683d83ebd4f0969839e5a
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-03-01 15:57:59 -0400

    Implement the FaviconBuilderUtils in the Service and move the build config to its own module
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

---
+++ src/modules/favicon-builder/favicon-builder.config.js
@@ -0,0 +1,32 @@
+/**
+ * Favicon Builder Configuration
+ * The configuration that will be used in order to generate the Favicon Build.
+ */
+const CONFIG = {
+  // the list of dimensions (in pixels) that will be generated and included in the build.
+  // important: this list should be kept in ascending order.
+  outputDimensions: [
+    { width: 16, height: 16 },
+    { width: 32, height: 32 },
+    { width: 48, height: 48 },
+    { width: 64, height: 64 },
+    { width: 96, height: 96 },
+    { width: 112, height: 112 },
+    { width: 128, height: 128 },
+    { width: 144, height: 144 },
+    { width: 160, height: 160 },
+    { width: 176, height: 176 },
+    { width: 192, height: 192 },
+    { width: 208, height: 208 },
+    { width: 224, height: 224 },
+    { width: 240, height: 240 },
+    { width: 256, height: 256 },
+    { width: 384, height: 384 },
+    { width: 512, height: 512 },
+  ],
+};
+
+/**
+ * Module Exports
+ */
+export default CONFIG;
--- src/modules/favicon-builder/favicon-builder.service.js
+++ src/modules/favicon-builder/favicon-builder.service.js
@@ -1,144 +1,14 @@
-import sharp from 'sharp';
 import pngToIco from 'png-to-ico';
 import Utilities from '../shared/utilities/utilities.js';
 import FileSystemService from '../shared/file-system/file-system.service.js';
+import FaviconBuilderUtils from './favicon-builder.utils.js';
+import CONFIG from './favicon-builder.config.js';
 
 /**
  * Favicon Builder Service Factory
  * Service in charge of building all the assets required to include a Favicon in a web app.
  */
 const faviconBuilderServiceFactory = () => {
-  /* *******************
-   * MODULE PROPERTIES *
-   ******************* */
-
-  // the list of dimensions (in pixels) that will be generated and included in the build.
-  // important: this list should be kept in ascending order.
-  const __OUTPUT_DIMENSIONS = [
-    { width: 16, height: 16 },
-    { width: 32, height: 32 },
-    { width: 48, height: 48 },
-    { width: 64, height: 64 },
-    { width: 96, height: 96 },
-    { width: 112, height: 112 },
-    { width: 128, height: 128 },
-    { width: 144, height: 144 },
-    { width: 160, height: 160 },
-    { width: 176, height: 176 },
-    { width: 192, height: 192 },
-    { width: 208, height: 208 },
-    { width: 224, height: 224 },
-    { width: 240, height: 240 },
-    { width: 256, height: 256 },
-    { width: 384, height: 384 },
-    { width: 512, height: 512 },
-  ];
-
-
-
-
-  /* **************
-   * MISC HELPERS *
-   ************** */
-
-  /**
-   * Generates the name of a favicon file based on its dimensions.
-   * @param {*} dimensions
-   * @returns string
-   */
-  const __getFaviconNameByDimensions = (dimensions) => `${Utilities.prettifyImageDimensions(dimensions)}.png`;
-
-  /**
-   * Builds the path for a favicon based on its dimension.
-   * @param {*} buildID
-   * @param {*} dimensions
-   * @returns string
-   */
-  const __getFaviconPathByDimensions = (buildID, dimensions) => `${buildID}/favicons/${__getFaviconNameByDimensions(dimensions)}`;
-
-  /**
-   * Reads the file from the source path, ensures it meets the dimensions requirement and returns
-   * the instance.
-   * @param {*} sourcePath
-   * @returns Promise<Sharp>
-   */
-  const __readSourcePath = async (sourcePath) => {
-    // init the instance
-    const source = sharp(sourcePath);
-
-    // extract the metadata and ensure it matches the requirements
-    const metadata = await source.metadata();
-    const largestReq = __OUTPUT_DIMENSIONS.at(-1);
-    if (largestReq.width > metadata.width || largestReq.height > metadata.height) {
-      throw new Error(`The dimensions of the source file should be: ${Utilities.prettifyImageDimensions(largestReq)} minimum`);
-    }
-
-    // return the instance of the source file
-    return source;
-  };
-
-
-
-
-
-  /* ***************
-   * BUILD RECEIPT *
-   *************** */
-
-  /**
-   * Generates the list of favicon variations created during the build.
-   * @param {*} prefix
-   * @returns string
-   */
-  const __generateFaviconVariationsReceipt = (prefix) => __OUTPUT_DIMENSIONS.reduce(
-    (prev, current, index) => `${prev}${index !== 0 ? '\n' : ''}${prefix}${__getFaviconNameByDimensions(current)}`,
-    '',
-  );
-
-  /**
-   * Builds the receipt once all the build files have been generated.
-   * @param {*} sourcePath
-   * @param {*} buildID
-   * @returns string
-   */
-  const __generateBuildReceipt = (sourcePath, buildID) => {
-    // init the receipt
-    let receipt = buildID.toUpperCase();
-    receipt += `\n${Utilities.formatDate()}`;
-
-    // input
-    receipt += '\n\nINPUT:';
-    receipt += `\n${sourcePath}`;
-
-    // output
-    receipt += '\n\nOUTPUT:';
-    receipt += `\n${buildID}`;
-    receipt += '\n    favicon.ico';
-    receipt += '\n    source.png';
-    receipt += '\n    favicons/';
-    receipt += `\n${__generateFaviconVariationsReceipt('        ')}`;
-
-    // usage
-    const smallestReq = __OUTPUT_DIMENSIONS[0];
-    const largestReq = __OUTPUT_DIMENSIONS.at(-1);
-    receipt += '\n\nUSAGE:';
-    receipt += '\n1) Include the favicon.ico into the project\'s root';
-    // eslint-disable-next-line no-template-curly-in-string
-    receipt += '\n2) Place the favicons directory inside of the project\'s root. Resulting in /${PROJECT_ROOT}/favicons';
-    receipt += '\n3) Include the following tags inside of the app\'s <head>...</head>:';
-    receipt += '\n<!-- Favicon -->';
-    receipt += `\n<link rel="icon" type="image/png" sizes="${Utilities.prettifyImageDimensions(smallestReq)}" href="favicons/${__getFaviconNameByDimensions(smallestReq)}">`;
-    receipt += '\n...';
-    receipt += `\n<link rel="icon" type="image/png" sizes="${Utilities.prettifyImageDimensions(largestReq)}" href="favicons/${__getFaviconNameByDimensions(largestReq)}">`;
-
-    // finally, return the receipt
-    return receipt;
-  };
-
-
-
-
-
   /* ***************
    * BUILD PROCESS *
    *************** */
@@ -150,7 +20,10 @@ const faviconBuilderServiceFactory = () => {
    */
   const build = async (sourcePath) => {
     // read the source path
-    const sourceFile = await __readSourcePath(sourcePath);
+    const sourceFile = await FaviconBuilderUtils.readSourcePath(
+      sourcePath,
+      CONFIG.outputDimensions.at(-1),
+    );
 
     // initialize the identifier
     const id = Utilities.generateBuildID('favicon');
@@ -163,10 +36,10 @@ const faviconBuilderServiceFactory = () => {
       await FileSystemService.makeDirectory(`${id}/favicons`);
 
       // generate all the output sizes
-      await Promise.all(__OUTPUT_DIMENSIONS.map((dim) => sourceFile.resize(
+      await Promise.all(CONFIG.outputDimensions.map((dim) => sourceFile.resize(
         dim.width,
         dim.height,
-      ).toFile(__getFaviconPathByDimensions(id, dim))));
+      ).toFile(FaviconBuilderUtils.getFaviconPathByDimensions(id, dim))));
 
       // create a copy of the source file
       await FileSystemService.copyFile(sourcePath, `${id}/source.png`);
@@ -178,7 +51,7 @@ const faviconBuilderServiceFactory = () => {
       // generate the receipt of the build
       await FileSystemService.writeFile(
         `${id}/receipt.txt`,
-        __generateBuildReceipt(sourcePath, id),
+        FaviconBuilderUtils.generateBuildReceipt(sourcePath, id, CONFIG.outputDimensions),
       );
 
       // finally, return the build id
