973e58546e86e973f8c928aa7c75407744c6fb7a
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-03-18 19:02:20 -0400

    Initialize the integration tests for the PWAAssetsBuilderService
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/modules/pwa-assets-builder/pwa-assets-builder.service.test-integration.js
+++ src/modules/pwa-assets-builder/pwa-assets-builder.service.test-integration.js
@@ -1,8 +1,33 @@
-import { describe, test, afterAll, afterEach, beforeAll, beforeEach, expect } from '@jest/globals';
-import PWAAssetsBuilderService from './pwa-assets-builder.service.js';
+import { describe, test, afterAll, afterEach, beforeAll, beforeEach, expect, jest } from '@jest/globals';
+import FileSystemService from '../shared/file-system/file-system.service.js';
+import CONFIG from './pwa-assets-builder.config.js';
+import Service from './pwa-assets-builder.service.js';
 
 
-describe('Test suite template', () => {
+/**
+ * PWA Assets Builder Config Mock
+ * Since the number of assets that are generated is significant, it will be reduced to only
+ * generating the first and last images.
+ */
+jest.mock('./pwa-assets-builder.config.js', () => {
+  const originalModule = jest.requireActual('./pwa-assets-builder.config.js');
+  return {
+    __esModule: true,
+    default: {
+      ...originalModule.default,
+      output: {
+        icons: [
+          originalModule.default.output.icons[0],
+          originalModule.default.output.icons.at(-1),
+        ],
+      },
+    },
+  };
+});
+
+
+
+describe('Build Process', () => {
   beforeAll(() => { });
 
   afterAll(() => { });
@@ -11,7 +36,13 @@ describe('Test suite template', () => {
 
   afterEach(() => { });
 
-  test.skip('can calculate 2 plus 2', () => {
-    expect(2 + 2).toBe(4);
+  test('can build the PWA Assets', async () => {
+    const id = await Service.build('./pwa-assetsample.png', '#0C0C0C');
+    const outputPath = `${id}/pwa-assets`;
+    await expect(FileSystemService.pathExists(id)).resolves.toBeTruthy();
+    await expect(FileSystemService.pathExists(`${id}/source.png`)).resolves.toBeTruthy();
+    await expect(FileSystemService.pathExists(outputPath)).resolves.toBeTruthy();
+
+    await FileSystemService.deleteDirectory(id);
   });
 });
